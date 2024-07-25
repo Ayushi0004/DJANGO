6f4cdee0b7e7780c3bfaa86a9bdf2897f91eb6c4
ayushi0004 <ayushichaudhari404@gmail.com>
2024-07-25 12:09:09 +0530

    new

---
+++ index.js
@@ -0,0 +1,21 @@
+console.log("My first javascript program")
+
+num = 3456;
+console.log(num);
+console.log(typeof num)
+
+name="ayushi"
+console.log(name)
+console.log(typeof name)
+
+num1=8
+num2=6
+console.log(num1+num2);
+
+num3=68
+num4="56"
+console.log(num3+num4);
+console.log(1+2);
+
+console.log(1+'1'+1);
+console.log(1+1+'1');
