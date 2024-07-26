9d16038b3a5026f8b52b175bad76392270e357d9
ayushi0004 <ayushichaudhari404@gmail.com>
2024-07-26 12:55:11 +0530

    boot

---
+++ keyword.js
@@ -0,0 +1,12 @@
+num=246
+console.log(num);
+
+let num1=264
+console.log(num1);// in let we reassign the value 
+
+var num2=123
+console.log(num2);//it is older keyword which is not ussed in present.
+
+
+const num3=456
+console.log(num3)//in this we cant not change the value 
